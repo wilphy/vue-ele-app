4a3160a0d30d2bba0efdb9e11cd56b349c1219b6
wilphy <149201216@qq.com>
2020-06-27 22:52:08 +0800

    feat：高德地图API的调用与地址展示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,58 @@
 <template>
-  <div class="home">主页</div>
+  <div class="home">
+    <div class="header">
+      <div class="address_map">
+        <i class="fa fa-map-marker"></i>
+        <span>{{ address }}</span>
+        <i class="fa fa-sort-desc"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "home",
+  computed: {
+    address() {
+      return this.$store.getters.address
+    },
+  },
 }
 </script>
 
-<style></style>
+<style>
+.home {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  box-sizing: border-box;
+}
+.header,
+.search_wrap {
+  background-color: #009eef;
+  padding: 10px 16px;
+}
+.header .address_map {
+  color: #fff;
+  font-weight: bold;
+}
+.address_map i {
+  margin: 0 3px;
+  font-size: 18px;
+}
+.address_map span {
+  display: inline-block;
+  width: 80%;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.fa-map-marker {
+  padding-right: 5px;
+}
+.fa-sort-desc {
+  position: relative;
+  top: -3px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
         port: 8080,
         https: false,
         hotOnly: false,
+        disableHostCheck: true,
         proxy: {
             // 配置跨域
             '/api': {
